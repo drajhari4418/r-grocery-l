32951252e4c6cd16e6e95e25d4cde21794630da6
Jai Sharma <sendtojsharma@gmail.com>
2021-12-08 18:11:42 +0530

    adding images for each item list

--- src/List.js
+++ src/List.js
@@ -1,33 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
+
+import { createApi } from 'unsplash-js'
+
+const unsplash = createApi({
+  accessKey: 'aStwN3JIV45U7Srk5BgmFpTiH2qXLvsnhsqOY8juxoE',
+})
+
+const ListItem = ({ id, title, removeItem, editItem }) => {
+  const [titleImage, setTitleImage] = useState(null)
+  const titleImageRes = unsplash.search.getPhotos({
+    query: title,
+    page: 1,
+    perPage: 1,
+  })
+
+  titleImageRes.then((res) => {
+    setTitleImage(res.response.results[0].urls.full)
+  })
+
+  return (
+    <div>
+      <article className="grocery-item" key={id}>
+        <p className="title">{title}</p>
+
+        <div className="btn-container">
+          <button
+            type="button"
+            className="edit-btn"
+            onClick={() => editItem(id)}
+          >
+            <FaEdit />
+          </button>
+          <button
+            type="button"
+            className="delete-btn"
+            onClick={() => removeItem(id)}
+          >
+            <FaTrash />
+          </button>
+        </div>
+      </article>
+      {titleImage && <img w="8rem" h="4rem" alt={title} src={titleImage} />}
+    </div>
+  )
+}
 
 const List = ({ items, removeItem, editItem }) => {
   return (
     <div className="grocery-list">
-      {items.map((item) => {
-        const { id, title } = item
-        return (
-          <article className="grocery-item" key={id}>
-            <p className="title">{title}</p>
-            <div className="btn-container">
-              <button
-                type="button"
-                className="edit-btn"
-                onClick={() => editItem(id)}
-              >
-                <FaEdit />
-              </button>
-              <button
-                type="button"
-                className="delete-btn"
-                onClick={() => removeItem(id)}
-              >
-                <FaTrash />
-              </button>
-            </div>
-          </article>
-        )
-      })}
+      {items.map((item) => (
+        <ListItem
+          id={item.id}
+          title={item.title}
+          removeItem={removeItem}
+          editItem={editItem}
+        />
+      ))}
     </div>
   )
 }
